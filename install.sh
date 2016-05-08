a557a141698f4c9460809db54ae25606815381ea
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-09 01:58:24 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
-if [-e ~/github]; then
+if [ -e ~/github ]; then
+  echo "~/github exists"
+else
   echo "mkdir ~/github"
   mkdir ~/github
-else
-  echo "~/github exists"
 fi
 
-if [! -e ~/github/env]; then
-  git clone https://github.com/sotetsuk/env
+if [ ! -e ~/github/env ]; then
+  cd ~/github; git clone https://github.com/sotetsuk/env
 else
-  echo "~/github/env exists"
+  cd ~/github/env; git pull;
 fi
 
 echo "./sh ~/github/env/brew.sh"
-./sh ~/github/env/brew.sh
+sh ~/github/env/brew.sh
 
-if cat ~/.zshrc | grep alias.sh == 0; then
+if cat ~/.zshrc | grep "alias.sh" -eq 0; then
   echo "setting alias"
   echo "\n# import alias setting\nsource ~/github/env/alias.sh" >> ~/.zshrc
 fi
 
-if cat ~/.zshrc | grep path.sh == 0; then
+if cat ~/.zshrc | grep "path.sh" -eq 0; then
   echo "setting path"
   echo "\n# import path setting\nsource ~/github/env/path.sh" >> ~/.zshrc
 fi
