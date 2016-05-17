06937c93b85085c6f7299d39410f7318dbc6277c
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-17 22:57:46 +0900

    add alias

--- alias.sh
+++ alias.sh
@@ -1,3 +1,6 @@
 alias ls="ls -alGh"
 alias fuck="networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on"
 alias p8="ping 8.8.8.8"
+alias openvpn="scutil --nc start kuins_lab"
+alias closevpn="scutil --nc stop kuins_lab"
+alias vnczaou="open vnc://h75.11.232.10.1017345.vlan.kuins.net"
