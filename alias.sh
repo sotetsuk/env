827ec6359231065a0f2dc617810138d05a89bfea
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-06-22 13:39:55 +0900

    add duo()

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 alias ls="ls -alGh"
 alias fuck="networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on"
 alias p8="ping 8.8.8.8"
+function duo() {flashcard --hint $1 --num 10 --random https://docs.google.com/spreadsheets/d/1bHXt4ZY0S54RU6EeT7eHGh_KAI7ncdn96KSh-Hx0xGo}
