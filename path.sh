78b1a57688aef640aeec81e9c996df8ba528973a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-07-01 16:14:06 +0900

    add path to GAE/GO SDK

--- path.sh
+++ path.sh
@@ -9,3 +9,6 @@ if [ ! -e ~/gopath ]; then
 fi
 export GOPATH=~/gopath
 export PATH=$GOPATH/bin:$PATH
+
+# GAE/GO sdk (download from https://cloud.google.com/appengine/downloads#Google_App_Engine_SDK_for_Go)
+export PATH=~/go_appengine:$PATH
