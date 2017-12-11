8a206410f29b35fea60b7b10f1b7e9d51f1b5b9d
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2017-12-11 11:27:40 +0900

    update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,7 +1,4 @@
 alias ls="ls -alGh"
 alias fuck="networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on"
 alias p8="ping 8.8.8.8"
-alias openvpn="scutil --nc start kuins_lab"
-alias closevpn="scutil --nc stop kuins_lab"
-alias vnczaou="open vnc://h75.11.232.10.1017345.vlan.kuins.net"
-function duo() {flashcard --hint $1 --num 10 --random https://docs.google.com/spreadsheets/d/1bHXt4ZY0S54RU6EeT7eHGh_KAI7ncdn96KSh-Hx0xGo}
+alias slp="osascript -e 'tell application \"Finder\" to sleep'"
