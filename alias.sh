5e7e5a84b974a1be31f5449e2a9f91c9682aab2f
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2018-07-21 00:01:29 +0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,4 +1,5 @@
-alias ls="ls -alGh"
 alias fuck="networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on"
 alias p8="ping 8.8.8.8"
 alias slp="osascript -e 'tell application \"Finder\" to sleep'"
+# https://havelog.ayumusato.com/develop/others/e523-pretty_cat_and_less_pygments.html
+alias c='pygmentize -O style=monokai -f console256 -g'
