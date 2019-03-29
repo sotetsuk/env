61e2189b3ba8e4d43c20fc6a2362d218707efc5b
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2019-03-29 15:17:52 +0900

    remove ls alias

--- alias.sh
+++ alias.sh
@@ -1,4 +1,3 @@
-alias ls="ls -alGh"
 alias fuck="networksetup -setairportpower en0 off; sleep 2; networksetup -setairportpower en0 on"
 alias p8="ping 8.8.8.8"
 alias slp="osascript -e 'tell application \"Finder\" to sleep'"
